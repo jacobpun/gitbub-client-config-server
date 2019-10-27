daae68b8c95792bff820d7c489e39689c61dec01
Punnoose <punnoosekuttyj@yahoo.com>
2019-10-27 12:58:59 -0400

    Initial commit

---
+++ src/main/java/com/pk/gitbubclientconfigserver/GithubClientConfigServer.java
@@ -0,0 +1,13 @@
+package com.pk.gitbubclientconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class GithubClientConfigServer {
+	public static void main(String[] args) {
+		SpringApplication.run(GithubClientConfigServer.class, args);
+	}
+}
